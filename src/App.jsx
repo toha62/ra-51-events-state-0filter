0c3bafb3e892ea082948423bea262a9886f9e581
Anton Trifonov <toha62@inbox.ru>
2023-04-12 09:56:18 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import Portfolio from './components/Portfolio';
 
 function App() {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
-
   return (
     <Portfolio />   
   )
